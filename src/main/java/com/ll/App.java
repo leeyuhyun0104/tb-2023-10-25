a15f054e4005ef2300442259abe761e883d5845f
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-10-25 22:03:47 +0900

    1단계 ì완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,7 +1,23 @@
 package com.ll;
 
+import java.util.Scanner;
+
 class App {
     void run() {
-        System.out.println("프로그램 실행");
+        while(true) {
+            System.out.println("== 명언 앱 ==");
+
+            System.out.print("명령) ");
+
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+
+            if(cmd.equals("종료")){
+                System.out.println("종료합니다.");
+                break;
+            }
+
+            System.out.printf("입력하신 명령 : %s\n", cmd);
+        }
     }
 }
