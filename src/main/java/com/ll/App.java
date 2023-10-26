17bdf280e74379b71e67cca61f5e75041b15fd09
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-10-26 11:24:46 +0900

    step 3, 4 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 class App {
     void run() {
+        int count = 0;
+
         while(true) {
             System.out.println("== 명언 앱 ==");
 
@@ -21,6 +23,9 @@ class App {
 
                 System.out.print("작가 : ");
                 String author= scanner.nextLine();
+
+                count++;
+                System.out.printf("%d번 명언이 등록되었습니다.\n", count);
             }
         }
     }
